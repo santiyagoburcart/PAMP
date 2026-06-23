7de695f32669511752befd8dcada04d9444bd5ff
PAMP <pamp@pasargad.local>
2026-06-23 01:57:47 +0000

    auto: update admins/views.py

--- admins/views.py
+++ admins/views.py
@@ -12,7 +12,7 @@ from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
 from django.conf import settings as dj_settings
 
-from .models import PanelAdmin, AdminLimit, SyncLog, SyncSettings, _fmt_bytes
+from .models import PanelAdmin, SyncLog, SyncSettings, _fmt_bytes
 from .tasks import sync_panel_admins
 
 logger = logging.getLogger('admins')
