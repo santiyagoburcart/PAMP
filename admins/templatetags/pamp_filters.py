9619db16659212fb1681be7ab1a3f15852cd8b76
PAMP <pamp@pasargad.local>
2026-06-23 01:39:36 +0000

    auto: update admins/templatetags/pamp_filters.py

--- admins/templatetags/pamp_filters.py
+++ admins/templatetags/pamp_filters.py
@@ -6,3 +6,9 @@ register = template.Library()
 @register.filter
 def split(value, sep=' '):
     return value.split(sep)
+
+
+@register.filter
+def get_item(dictionary, key):
+    """Look up a dict value by a dynamic key: {{ my_dict|get_item:variable }}"""
+    return dictionary.get(key)
