97597e99f015ea83cb3afc20f76ece946c567fb4
PAMP <pamp@pasargad.local>
2026-09-05 19:49:58 +0000

    auto: update admins/context_processors.py

--- admins/context_processors.py
+++ admins/context_processors.py
@@ -10,7 +10,22 @@ def pamp_globals(request):
             version = f.read().strip()
     except Exception:
         version = 'dev'
+
+    last_sync_fmt = None
+    avatar_url = ''
+    if request.user.is_authenticated:
+        try:
+            from admins.models import SyncLog
+            log = SyncLog.objects.order_by('-id').first()
+            if log:
+                last_sync_fmt = log.started_at.strftime('%H:%M')
+        except Exception:
+            pass
+        avatar_url = request.session.get('avatar_url', '')
+
     return {
         'pamp_version': version,
         'github_url': 'https://github.com/santiyagoburcart/PAMP',
+        'last_sync_fmt': last_sync_fmt,
+        'avatar_url': avatar_url,
     }
